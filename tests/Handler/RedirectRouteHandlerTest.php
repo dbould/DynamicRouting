322f0a844c7e5744b5c59cad5e46a190e2645436
dave <dave@bould.net>
2016-10-18 18:59:09 +0100

    Added tests for RedirectRouteHandler

---
+++ tests/Handler/RedirectRouteHandlerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Handler;
+
+use ElementsFramework\DynamicRouting\Model\DynamicRoute;
+use ElementsFramework\DynamicRouting\Service\RouteDispatcher;
+use ElementsFramework\DynamicRouting\Handler\RedirectRouteHandler;
+use Illuminate\Http\Request;
+use Tests\BaseTest;
+
+class RedirectRouteHandlerTest extends BaseTest
+{
+    public function testProcessWithRedirect()
+    {
+        $route = new DynamicRoute();
+        $route->fill([
+            'name' => 'test',
+            'pattern' => '/test',
+            'method' => 'get',
+            'handler' => 'RedirectRouteHandler',
+            'configuration' => ['target' => '/', 'status_code' => 302],
+        ]);
+
+        $redirectRoute = new RedirectRouteHandler();
+        $response = $redirectRoute->process(new Request, $route);
+
+        $this->assertEquals(302, $response->getStatusCode());
+    }
+
+    public function testIsValidTargetFalse()
+    {
+
+        $route = new DynamicRoute();
+        $route->fill([
+            'name' => 'test',
+            'pattern' => '/test',
+            'method' => 'get',
+            'handler' => 'RedirectRouteHandler',
+            'configuration' => [],
+        ]);
+
+        $redirectRoute = new RedirectRouteHandler();
+
+        $this->assertEquals(false, $redirectRoute->isValid($route));
+    }
+}
